f693dc33177dd0e0cc3fdb038b21252e33fa6ca6
zzgo <zzgoCC@gmail.com>
2017-06-19 09:28:26 +0800

    + add ReverseInteger

---
+++ ReverseInteger/ReverseInteger.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func reverse(_ x: Int) -> Int {
+    
+     var res = 0
+        var x = x
+  
+        while x != 0 {
+            res = res * 10 + x % 10
+            x = x / 10
+            
+            if res > Int(Int32.max) || res < Int(Int32.min) {
+                return 0
+            }
+        }
+        
+        return res
+    }
+}
