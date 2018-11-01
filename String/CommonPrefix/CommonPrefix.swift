7cbc0e3410b41b71e49038b2d93a627a0558b86c
zenon <zzgocc@gmail.com>
2018-11-01 23:58:23 +0800

    优化 - 公共最长前缀,效率提高 10 倍

--- String/CommonPrefix/CommonPrefix.swift
+++ String/CommonPrefix/CommonPrefix.swift
@@ -88,5 +88,121 @@ class Solution {
     }
 }
 
+//Pass: 28 ms 优化后
+//优化点在于,得到第一个前缀后，根据前缀的长度/字符串的长度，得到一个最小的长度。每次直接用最小长度去比较，不断逼近结果
+//之前的思路，是不断地从第一个比较到后面，同样的1000个字符，全部同样的前缀3个字符，则需要：（3+1）*1000 = 4000 次比较，新的方案，只需要 4 + 998 次。
+class Solution {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        
+        if(strs.count == 1 ){
+            return strs[0]
+        }
+        
+        var strList = strs
+        
+        var commonPrefix : String = ""
+        
+        if(strs.count>=2){
+            var zeroStr : String = strList[0]
+            var firstStr : String = strList[1]
+            
+            if(zeroStr.count<=0){
+                return ""
+            }
+            
+            if(firstStr.count<=0){
+                return ""
+            }
+            
+            var length : Int = 0
+            var  needCompare : Bool = true
+            
+            while (needCompare){
+                
+                if(length>zeroStr.count){
+                    break
+                }
+                if(length>firstStr.count){
+                    break
+                }
+                
+                var offsetIndex =  zeroStr.index( zeroStr.startIndex, offsetBy: length)
+                var subString =  zeroStr[..<offsetIndex]
+                var compareStr : String = String(subString)
+                
+                
+                offsetIndex =   firstStr.index( firstStr.startIndex, offsetBy: length)
+                subString =  firstStr[..<offsetIndex]
+                let compareToStr : String = String(subString)
+                
+                if(compareStr==compareToStr){
+                    
+                    commonPrefix = compareStr
+                    
+                    length = length + 1
+                    
+                }else{
+                    needCompare = false
+                    if(length==1){// 1个相同的字符前缀都没有
+                        return ""
+                    }
+                }
+            }
+            
+            print("first \(commonPrefix)")
+            
+            strList.remove(at: 1)
+            strList.remove(at: 0)
+            
+        }
+        
+        for str in strList{
+            
+            if(str.count<1){
+                return ""
+            }
+            
+            var length : Int = 0
+            if (commonPrefix.count < str.count){ 
+                length = commonPrefix.count
+            }else {
+                length = str.count
+            }
+            
+            while (length>0){
+                
+                var offsetIndex =  str.index( str.startIndex, offsetBy: length)
+                var subString =  str[..<offsetIndex]
+                var compareStr : String = String(subString)
+                
+                //裁剪公共前缀
+                offsetIndex =  commonPrefix.index( commonPrefix.startIndex, offsetBy: length)
+                subString =  commonPrefix[..<offsetIndex]
+                
+                commonPrefix = String(subString)
+                print("compare item \(compareStr)")
+                
+                print("commonPrefix \( commonPrefix)")
+                
+                if(compareStr==commonPrefix){
+                    
+                    break
+                }else{
+                    
+                    length = length - 1
+                    if(length==0){
+                        return ""
+                    }
+                }
+                
+            }
+            
+        }
+        
+        
+        return commonPrefix
+    }
+}
+
 //TODO
 //改进思路： 使用 字典/Set 集合，记录字符次数。出现次数和整个数组大小相等，为公共字符。最后拼凑公共字符
