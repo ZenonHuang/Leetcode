ac9e14adf3584976f677cd058dc40e09190d9859
zenon <zzgocc@gmail.com>
2021-02-26 21:11:00 +0800

    反转字符串-击败 98.21%

--- String/ReverseString/ReverseString.swift
+++ String/ReverseString/ReverseString.swift
@@ -1,3 +1,22 @@
+//2021/2/26  
+//216 ms ,时间击败了98.21%
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+
+        let count:Int = s.count
+        let avr = s.count / 2
+
+        var temp: Character = " "
+        for i in 0..<avr {
+            temp = s[i]
+            s[i] = s[count - i - 1]
+            s[count - i - 1] = temp
+        }
+        
+    }
+}
+
+
 // Pass : 136 ms
 class Solution {
     func reverseString(_ s: String) -> String {
