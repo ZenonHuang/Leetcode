0825180e155bad4e02da1c9540ae2b584279849e
zenon <zzgocc@gmail.com>
2021-03-20 10:58:43 +0800

    更新: 移动 0 ，击败 100%

--- Array/MoveZeroes/moveZeroes.swift
+++ Array/MoveZeroes/moveZeroes.swift
@@ -1,3 +1,26 @@
+//Pass : 40 ms,击败 100%
+class Solution {
+    func moveZeroes(_ nums: inout [Int]) {
+        if(nums.isEmpty) {
+            return;
+        }
+        //两个指针i和j
+        var j = 0;
+        for i in 0..<nums.count {
+            //当前元素!=0，就把其交换到左边，等于0的交换到右边
+            if nums[i] != 0 {
+                
+                if (i > j) {// 避免了数组开头是非零元素的交换也就是阻止（i==j）时交换
+                 nums[j] = nums[i];
+                 nums[i] = 0;
+                }
+                j = j + 1
+            }
+        }
+
+    }
+}
+
 //Pass : 164 ms
 class Solution {
     func moveZeroes(_ nums: inout [Int]) {
